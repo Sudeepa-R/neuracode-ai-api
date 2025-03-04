040b90d82c584b689b1c88b7c2823f7840188b12
Sudeepa-R <sudeepar084@gmail.com>
2025-03-04 20:41:12 +0530

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NeuraCode AI API')
+    .setDescription('API documentation for NeuraCode AI')
+    .setVersion('1.0')
+    .addTag('Controllers')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/', app, document);
 
   const PORT = process.env.PORT;
   await app.listen(PORT ?? 3000);
