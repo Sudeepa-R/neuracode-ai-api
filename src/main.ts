fb16295dce5b2eb9aea96c03765b7d52c80654d9
Sudeepa-R <sudeepar084@gmail.com>
2025-03-04 20:19:52 +0530

    package-lock.json file removed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+import { Logger } from '@nestjs/common';
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const PORT = process.env.PORT;
+  await app.listen(PORT ?? 3000);
+  Logger.log(` 🚀 Server running at http://localhost:${PORT}`);
 }
 bootstrap();
