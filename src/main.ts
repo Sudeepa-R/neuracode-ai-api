70c3baeb2b5a3635d2c81d687b1bf315eb677bb7
Sudeepa-R <sudeepar084@gmail.com>
2025-03-05 19:31:57 +0530

    compression and reatelimit added

--- src/main.ts
+++ src/main.ts
@@ -3,49 +3,74 @@ import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import { Logger } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import rateLimit from 'express-rate-limit';
+import * as compression from 'compression';
 config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  try {
+    const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('NeuraCode AI API')
-    .setDescription('API documentation for NeuraCode AI')
-    .setVersion('1.0')
-    .addTag('Controllers')
-    .addBearerAuth()
-    .build();
-  
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
-    customfavIcon: 'https://petstore.swagger.io/favicon-32x32.png',
-    customCssUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
-    customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
-    ],
-    swaggerOptions: {
-      deepLinking: false, // Disables `#` in URLs
-      defaultModelsExpandDepth: -1, // Hides schemas/models section
-    },
-  });
-  SwaggerModule.setup('/', app, document, {
-    customfavIcon: 'https://petstore.swagger.io/favicon-32x32.png',
-    customCssUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
-    customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
-    ],
-    swaggerOptions: {
-      deepLinking: false, // Disables `#` in URLs
-      defaultModelsExpandDepth: -1, // Hides schemas/models section
-    },
-  });
+    app.use(
+      rateLimit({
+        windowMs: 1000, // 1 second
+        max: 100, // limit each IP to 100 request per windows
+        message: 'Too many requests. Please try again later.',
+      }),
+    );
 
-  const PORT = process.env.PORT;
-  await app.listen(PORT ?? 3000);
-  Logger.log(` 🚀 Server running at http://localhost:${PORT}`);
+    app.use(compression());
+
+    const config = new DocumentBuilder()
+      .setTitle('NeuraCode AI API')
+      .setDescription('API documentation for NeuraCode AI')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .addServer('/api')
+      .addServer('/')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document, {
+      customfavIcon: 'https://petstore.swagger.io/favicon-32x32.png',
+      customCssUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+      customJs: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      ],
+      swaggerOptions: {
+        deepLinking: false, // Disables `#` in URLs
+        // defaultModelsExpandDepth: -1, // Hides schemas/models section,
+        docExpansion: 'none',
+        tagsSorter: 'alpha',
+      },
+    });
+    SwaggerModule.setup('/', app, document, {
+      customfavIcon: 'https://petstore.swagger.io/favicon-32x32.png',
+      customCssUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+      customJs: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      ],
+      swaggerOptions: {
+        deepLinking: false, // Disables `#` in URLs
+        // defaultModelsExpandDepth: -1, // Hides schemas/models section
+        docExpansion: 'none',
+        tagsSorter: 'alpha',
+      },
+    });
+
+    const PORT = process.env.PORT;
+    await app.listen(PORT ?? 3000);
+    Logger.log(` 🚀 Server running at http://localhost:${PORT}`);
+  } catch (err) {
+    Logger.error(`❌ Error starting server : ${err}`, '', 'Boostrap', false);
+    process.exit(1);
+  }
 }
-bootstrap();
+bootstrap().catch((err) => {
+  Logger.error(`❌ Error starting server : ${err}`, '', 'Boostrap', false);
+  throw err;
+});
